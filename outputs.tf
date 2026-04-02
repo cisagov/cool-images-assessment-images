190cd6cd53c8b33665fa80a6749d3387b10e4bd2
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2026-04-02 15:14:39 -0400

    Add an output for the new bucket read-only role

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "assessmentimagesbucketfullaccess_role" {
   value       = aws_iam_role.fullaccess
 }
 
+output "assessmentimagesbucketreadonly_role" {
+  description = "The IAM role that allows read-only access to the assessment images bucket in the Images account."
+  value       = aws_iam_role.readonly
+}
+
 output "read_terraform_state" {
   description = "The IAM policies and role that allow read-only access to the cool-images-assessment-images state in the Terraform state bucket."
   value       = module.read_terraform_state
