7ec6ecd91743677e51d0ac1dcca5b2fb3a13b975
dav3r <david.redmin@gwe.cisa.dhs.gov>
2026-04-07 13:52:38 -0400

    Allow all s3:GetObject* permissions
    Co-authored-by: Nick <50747025+mcdonnnj@users.noreply.github.com>

--- assessmentimagesbucketreadonly_policy.tf
+++ assessmentimagesbucketreadonly_policy.tf
@@ -16,9 +16,7 @@ data "aws_iam_policy_document" "readonly" {
 
   statement {
     actions = [
-      "s3:GetObject",
-      "s3:GetObjectTagging",
-      "s3:GetObjectVersion",
+      "s3:GetObject*",
     ]
     resources = [
       "${aws_s3_bucket.assessment_images.arn}/*"
