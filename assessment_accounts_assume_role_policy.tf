1f05a34c9fe731072b876cf948562b51dbed1c92
dav3r <david.redmin@gwe.cisa.dhs.gov>
2026-04-07 10:46:30 -0400

    Improve verbiage in a precondition error message
    Co-authored-by: Shane Frasier <jeremy.frasier@gwe.cisa.dhs.gov>

--- assessment_accounts_assume_role_policy.tf
+++ assessment_accounts_assume_role_policy.tf
@@ -19,7 +19,7 @@ data "aws_iam_policy_document" "assessment_account_assume_role_doc" {
   lifecycle {
     precondition {
       condition     = length(local.assessment_account_ids) > 0
-      error_message = "No assessment accounts were discovered. local.assessment_account_ids must contain at least one AWS account ID before generating the assume role policy."
+      error_message = "No assessment accounts were discovered. local.assessment_account_ids must contain at least one AWS account ID before creating the assume role policy."
     }
   }
 }
